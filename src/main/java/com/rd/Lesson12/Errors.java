3676ec65b9df472baa698d3e753ea9a43f1c4419
serkancura <serkan@serkancura.com>
2024-10-24 21:53:59 +0300

    Lesson11

---
+++ src/main/java/com/rd/Lesson12/Errors.java
@@ -0,0 +1,19 @@
+package com.rd.Lesson12;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Errors {
+    public static void main(String[] args) throws IOException {
+        String dosyaYolu = "src/main/java/com/rd/Lesson12/dosya.abc";
+
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(dosyaYolu));
+        String satir;
+
+        while ((satir = bufferedReader.readLine()) != null) {
+            System.out.println(satir);
+        }
+    }
+}
